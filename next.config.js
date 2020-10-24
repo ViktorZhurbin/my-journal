bdd25facd12a6893bf1ea6a852de70d4e4d2bb16
Viktor Zhurbin <vzhurbin@gmail.com>
2020-10-24 20:49:45 +0300

    add pwa support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withImages = require('next-images');
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
-module.exports = withImages();
+module.exports = withImages(
+    withPWA({
+        pwa: {
+            dest: 'public',
+            runtimeCaching,
+        },
+    })
+);
