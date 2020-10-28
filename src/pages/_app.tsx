b9abfb1faae299d402e3c55323d75cc762ff841a
Viktor Zhurbin <vzhurbin@gmail.com>
2020-10-28 12:17:30 +0300

    add typography js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
 import React, { ReactElement } from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
+import { typography } from '../styles/typography';
 import '../styles/global.css';
 
 export default function MyApp({
     Component,
     pageProps,
 }: AppProps): ReactElement {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <style
+                    id={'typography.js'}
+                    dangerouslySetInnerHTML={{
+                        __html: typography.toString(),
+                    }}
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
---
+++ src/styles/typography.ts
@@ -0,0 +1,4 @@
+import Typography from 'typography';
+import githubTheme from 'typography-theme-github';
+
+export const typography = new Typography(githubTheme);
